afb8aa614b69ad988dbd19e7d55ce46b3d71d25f
klaud_skora <klaud.skora@gmail.com>
2021-03-08 07:12:18 +0100

    Add routes

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const newsRouter = require('./routes/news');
+const quizRouter = require('./routes/news');
+const adminRouter = require('./routes/news');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/news', newsRouter);
+app.use('/quiz', quizRouter);
+app.use('/admi', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
