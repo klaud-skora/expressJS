b6f594826491879a804ab7d20f04b64423e736cb
klaud_skora <klaud.skora@gmail.com>
2021-03-14 20:02:58 +0100

    Add form validation

--- models/news.js
+++ models/news.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const NewsSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
+  title: { type: String, required: [true, 'Type title'] },
+  content: { type: String, required: [true, 'Type description'] },
   createdDt: { type: Date, default: Date.now },
 });
 
--- routes/news.js
+++ routes/news.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const news = require('../models/news');
+const News = require('../models/news');
+
 const router = express.Router();
 
 /* GET users listing. */
@@ -7,7 +10,32 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/add', (req, res, next) => {
+  if (!req.session.admin) res.redirect('news');
   res.render('addNews');
+});
+
+router.post('/add', (req, res) => {
+
+  const { title, content } = req.body;
+
+  const newDocument = new News({
+    title, 
+    content,
+  });
+
+  const errors = newDocument.validateSync();
+
+  if (errors) {
+    res.render('addNews', { errors, title, content });
+    return;
+  }
+
+  newDocument.save((err) => {
+    console.log(err);
+    if (!err) res.redirect('/news');
+    else res.render('addNews', { errors: { errors: ['Could not connect to db, sth went wrong'] }, title, content });
+  });
+  
 });
 
 module.exports = router;
